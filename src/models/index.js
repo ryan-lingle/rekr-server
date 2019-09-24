e3314e6d93264677e5825a6113774ba7e31770d0
Ryan Lingle <lingleryan@gmail.com>
2019-09-24 15:08:52 -0400

    edit db connection

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,10 @@ const db = {};
 
 let sequelize;
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, config);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
