2b9d04a7193b06abc401456a4e04310acafa348d
Ryan Lingle <lingleryan@gmail.com>
2020-01-19 17:58:41 -0600

    debug admin auth[

--- src/admin/controller.js
+++ src/admin/controller.js
@@ -9,6 +9,9 @@ module.exports = function adminController(app) {
   app.use(express.json());
 
   app.get("/admin/api", async (req, res) => {
+    console.log(req.headers);
+    console.log("secret:", process.env.ADMIN_SECRET);
+
     if (req.headers.admintoken !== process.env.ADMIN_SECRET)
       res.send(403);
     const data = await adminMetrics(req);
@@ -19,8 +22,4 @@ module.exports = function adminController(app) {
   app.post("/admin/error", async (req, res) => {
     errorLogger(req.body);
   });
-
-  // app.post("/admin/login", async (req, res) => {
-  //   console.log(req.headers);
-  // });
 };
