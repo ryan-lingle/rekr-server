d292e2f74023bd7bacbaa538a7aedf7a4cae1700
Ryan Lingle <lingleryan@gmail.com>
2020-01-19 16:52:04 -0600

    admin auth

--- src/admin/controller.js
+++ src/admin/controller.js
@@ -2,12 +2,15 @@ const cors = require('cors');
 const express = require('express');
 const adminMetrics = require('./metrics');
 const errorLogger = require('./error_logger');
+const { ForbiddenError } = require('apollo-server-express');
 
 module.exports = function adminController(app) {
   app.use(cors());
   app.use(express.json());
 
   app.get("/admin/api", async (req, res) => {
+    if (req.headers.admintoken !== process.env.ADMIN_SECRET)
+      res.send(403);
     const data = await adminMetrics(req);
     res.set("Access-Control-Allow-Credentials", true);
     res.send(data);
@@ -16,4 +19,8 @@ module.exports = function adminController(app) {
   app.post("/admin/error", async (req, res) => {
     errorLogger(req.body);
   });
+
+  // app.post("/admin/login", async (req, res) => {
+  //   console.log(req.headers);
+  // });
 };
